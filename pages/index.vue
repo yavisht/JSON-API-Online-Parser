0ea68f3d14ba91e0325660078e357d65e5633a85
Yavisht Katgara <yavisht.katgara@gmail.com>
2019-10-08 22:16:40 +1100

    Add textbox to index template

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-form-textarea
+          id="textarea"
+          v-model="text"
+          placeholder="Enter something..."
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea>
+      </b-col>
+      <b-col>
+        <b-form-textarea
+          id="textarea"
+          v-model="text"
+          placeholder="Enter something..."
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<style lang="scss" scoped>
+textarea {
+  width: 100%;
+}
+</style>
